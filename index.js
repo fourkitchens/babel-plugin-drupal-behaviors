a4b678504f8a996cf4bc71138e47cb6c697abdfa
Evan Willhite <evan@fourkitchens.com>
2019-10-11 16:35:31 -0500

    working

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const template = require('babel-template');
 
-const drupalBehavior = template('Drupal.behaviors.myBehavior = {attach: function (context, settings) {\nBODY;\n}};');
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+const randomNum = getRandomNumber(1000);
+
+const drupalBehavior = template('Drupal.behaviors.behavior' + randomNum + ' = {attach: function (context, settings) {\nBODY;\n}};');
 
 module.exports = function (babel) {
-	const t = babel.types;
+  const t = babel.types;
 
 	return {
     inherits: require("babel-plugin-transform-strict-mode"),
 		visitor: {
 			Program: {
-				exit: function (path) {
+				exit (path) {
 					if (!this.drupalBehavior) {
-						this.drupalBehavior = true;
+            this.drupalBehavior = true;
+            // console.log(path.node.body[0].expression.callee.body.directives)
 						const addBehavior = drupalBehavior({
-							BODY: path.node.body
-						});
-						addBehavior[1].expression.callee.body.directives = path.node.directives;
+              BODY: path.node.body
+            });
+
+            // console.log(path.get('body'))
+            // addBehavior.expression.callee.body.directives = path.node.directives;
 
 						path.replaceWith(
-							t.program(addBehavior)
+							t.program([addBehavior])
 						);
 					}
 					path.node.directives = [];
